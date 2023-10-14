92e36d04babedc8827da45a4b70739c6bd634567
Singgih Budi Purnadi <57170834+sindev08@users.noreply.github.com>
2023-10-14 23:26:21 +0700

    test : setup turbo repo

---
+++ apps/web/app/components/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Components() {
+  return (
+    <div className="h-screen w-full">
+      <div className="flex justify-center items-center">
+        <span className="text-3xl text-blue-600 font-bold">
+          Test Components
+        </span>
+      </div>
+    </div>
+  );
+}
---
+++ packages/ui/components/Button/index.tsx
@@ -0,0 +1,9 @@
+export const Button = () => {
+  return (
+    <button
+      type="button"
+      className="border border-indigo-500 bg-indigo-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline">
+      Primary
+    </button>
+  );
+};
